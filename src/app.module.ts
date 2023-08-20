418cbd154aa2c98ca1996e72dc7263ac5d3166cf
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-08-20 13:47:22 +0900

    rafactor: 드라이버 세팅
    https://docs.nestjs.com/graphql/quick-start#getting-started-with-graphql--typescript

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
-    RestaurantsModule,
     GraphQLModule.forRoot({
+      driver: ApolloDriver,
       autoSchemaFile: true,
     }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
