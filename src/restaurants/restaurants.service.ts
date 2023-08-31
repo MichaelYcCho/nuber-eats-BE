50ec14cd36780cbf052ede9ff458cb0f7b67b74f
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-09-01 01:25:15 +0900

    feat: Update 로직 마무리

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 
 @Injectable()
 export class RestaurantService {
@@ -19,5 +20,9 @@ export class RestaurantService {
     // create 는 js에서 생성하는 부분, save는 db에 저장하는 부분
     const newRestaurant = this.restaurants.create(createRestaurantDto);
     return this.restaurants.save(newRestaurant);
+  }
+  updateRestaurant({ id, data }: UpdateRestaurantDto) {
+    // update의 첫번째 인자는 업데이트할 매개인자(criteria : 기준), 두번째 인자는 업데이트할 데이터,
+    return this.restaurants.update(id, { ...data });
   }
 }
