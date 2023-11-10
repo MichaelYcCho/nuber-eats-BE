efce1641deb8d1dcae24ec1502b0291259091163
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-11-11 00:17:29 +0900

    feat: Token Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,45 +1,46 @@
-import { ApolloDriver } from '@nestjs/apollo';
-import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
+import { ApolloDriver } from '@nestjs/apollo'
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from 'joi'
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UsersModule } from './users/users.module'
+import { User } from './users/entities/user.entity'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod',
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.string().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_NAME: Joi.string().required(),
-      }),
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT, // +: string to number ("123123" 형식일경우에만)
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      synchronize: process.env.NODE_ENV !== 'prod',
-      logging: process.env.NODE_ENV !== 'prod',
-      entities: [User],
-    }),
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
-      autoSchemaFile: true,
-    }),
-    UsersModule,
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+            ignoreEnvFile: process.env.NODE_ENV === 'prod',
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.string().required(),
+                DB_USERNAME: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_NAME: Joi.string().required(),
+                SECRET_KEY: Joi.string().required(),
+            }),
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT, // +: string to number ("123123" 형식일경우에만)
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            synchronize: process.env.NODE_ENV !== 'prod',
+            logging: process.env.NODE_ENV !== 'prod',
+            entities: [User],
+        }),
+        GraphQLModule.forRoot({
+            driver: ApolloDriver,
+            autoSchemaFile: true,
+        }),
+        UsersModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
+import * as jwt from 'jsonwebtoken'
 import { CreateAccountInput } from './dtos/create-account.dto'
 import { LoginInput } from './dtos/login.dto'
 import { User } from './entities/user.entity'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class UserService {
-    constructor(@InjectRepository(User) private readonly users: Repository<User>) {}
+    constructor(
+        @InjectRepository(User) private readonly users: Repository<User>,
+        private readonly config: ConfigService,
+    ) {}
 
     async createAccount({ email, password, role }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
         try {
@@ -39,6 +44,7 @@ export class UserService {
                     error: 'Wrong password',
                 }
             }
+            const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'))
             return {
                 ok: true,
                 token: 'lalalalaalala',
