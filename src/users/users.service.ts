7c2248a1d3ecdec524cd1723436d9fa4ec03ef93
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-11-13 00:00:02 +0900

    feat: JWT Dynamic Module Example

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -46,7 +46,7 @@ export class UserService {
                     error: 'Wrong password',
                 }
             }
-            const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'))
+            const token = jwt.sign({ id: user.id }, this.config.get('PRIVATE_KEY'))
             return {
                 ok: true,
                 token: token,
