015482eddf2ca04f091d7c9b08d9289e2408de55
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-11-21 23:00:02 +0900

    feat: UserProfile Mutation

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -6,6 +6,7 @@ import { UserService } from './users.service'
 import { UseGuards } from '@nestjs/common'
 import { AuthGuard } from 'src/auth/auth.guard'
 import { AuthUser } from 'src/auth/auth-user.decorator'
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto'
 
 @Resolver((of) => User)
 export class UserResolver {
@@ -49,5 +50,25 @@ export class UserResolver {
     @UseGuards(AuthGuard)
     me(@AuthUser() authUser: User) {
         return authUser
+    }
+
+    @UseGuards(AuthGuard)
+    @Query((returns) => UserProfileOutput)
+    async userProfile(@Args() userProfileInput: UserProfileInput): Promise<UserProfileOutput> {
+        try {
+            const user = await this.usersService.findById(userProfileInput.userId)
+            if (!user) {
+                throw Error()
+            }
+            return {
+                ok: true,
+                user,
+            }
+        } catch (e) {
+            return {
+                error: 'User Not Found',
+                ok: false,
+            }
+        }
     }
 }
