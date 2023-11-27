d50972df0bf061bd418125d5b604b0a007324488
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-11-27 23:34:12 +0900

    feat: Verifying User

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -8,6 +8,7 @@ import { AuthGuard } from 'src/auth/auth.guard'
 import { AuthUser } from 'src/auth/auth-user.decorator'
 import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto'
 import { EditProfileInput, EditProfileOutput } from './src/users/dtos/edit-profile.dto'
+import { VerifyEmailInput, VerifyEmailOutput } from './src/users/dtos/verify-email.dto'
 
 @Resolver((of) => User)
 export class UserResolver {
@@ -90,5 +91,10 @@ export class UserResolver {
                 error,
             }
         }
+    }
+
+    @Mutation((returns) => VerifyEmailOutput)
+    verifyEmail(@Args('input') { code }: VerifyEmailInput) {
+        this.usersService.verifyEmail(code)
     }
 }
