9257f4a9d1254b4b4d233bdccc6b67961ab4d034
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-12-02 23:00:02 +0900

    feat: MailGun Template

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -15,7 +15,9 @@ export class MailService {
         form.append('from', `Excited User <mailgun@${this.options.domain}>`)
         form.append('to', `test@naver.com`)
         form.append('subject', subject)
-        form.append('text', content)
+        form.append('template', 'verify-email')
+        form.append('v:code', 'asasas')
+        form.append('v:username', 'michael!!!')
         const response = await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
             method: 'POST',
             headers: {
