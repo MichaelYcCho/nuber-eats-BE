6bff35af0d7db52da2752c99587a356048840a79
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-12-07 23:33:02 +0900

    test: User Exist Unit Test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -60,7 +60,22 @@ describe('UserService', () => {
     })
 
     describe('createAccount', () => {
-        it('should fail if user exists', () => {})
+        it('should fail if user exists', async () => {
+            // findOne에 대한 mockResolvedValue를 설정한다.
+            usersRepository.findOne.mockResolvedValue({
+                id: 1,
+                email: '',
+            })
+            const result = await service.createAccount({
+                email: '',
+                password: '',
+                role: 0,
+            })
+            expect(result).toMatchObject({
+                ok: false,
+                error: 'There is a user with that email already',
+            })
+        })
     })
 
     // 테스트할 함수명을 작성
