c79f2a24b93ad62333d216cab44a8809f2f0d0b2
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-12-21 00:03:22 +0900

    test: CreateAccount E2E Test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,7 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
+import request from 'supertest'
 import { AppModule } from '../src/app.module'
 import { DataSource } from 'typeorm'
+
+// got가 실제로 호출되는 것을 방지하기 위해 mock을 사용(여기선 이메일 전송을 막기 위해)
+jest.mock('got', () => {
+    return {
+        post: jest.fn(),
+    }
+})
 
 const GRAPHQL_ENDPOINT = '/graphql'
 
@@ -31,7 +39,56 @@ describe('UserModule (e2e)', () => {
         await app.close()
     })
 
-    it.todo('can createAccount')
+    describe('createAccount', () => {
+        const EMAIL = 'michael@las.com'
+        it('should create account', () => {
+            return request(app.getHttpServer())
+                .post(GRAPHQL_ENDPOINT)
+                .send({
+                    query: `
+                        mutation {
+                            createAccount(input: {
+                            email:"${EMAIL}",
+                            password:"12345",
+                            role:Owner
+                            }) {
+                            ok
+                            error
+                            }
+                        }
+                        `,
+                })
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.data.createAccount.ok).toBe(true)
+                    expect(res.body.data.createAccount.error).toBe(null)
+                })
+        })
+
+        it('should fail if account already exists', () => {
+            return request(app.getHttpServer())
+                .post(GRAPHQL_ENDPOINT)
+                .send({
+                    query: `
+                        mutation {
+                            createAccount(input: {
+                            email:"${EMAIL}",
+                            password:"12345",
+                            role:Owner
+                            }) {
+                            ok
+                            error
+                            }
+                        }
+                        `,
+                })
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.data.createAccount.ok).toBe(false)
+                    expect(res.body.data.createAccount.error).toBe('There is a user with that email already')
+                })
+        })
+    })
     it.todo('userProfile')
     it.todo('login')
     it.todo('me')
