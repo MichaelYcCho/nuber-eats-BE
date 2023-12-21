96a329e5d88c3d7d150238e82c817d5c65e5e725
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-12-22 00:01:55 +0900

    test: Login E2E Test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -13,8 +13,14 @@ jest.mock('got', () => {
 
 const GRAPHQL_ENDPOINT = '/graphql'
 
+const testUser = {
+    email: 'michael@las.com',
+    password: '12345',
+}
+
 describe('UserModule (e2e)', () => {
     let app: INestApplication
+    let jwtToken: string
 
     beforeAll(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -39,6 +45,7 @@ describe('UserModule (e2e)', () => {
         await app.close()
     })
 
+    // 유저 생성 e2e 테스트
     describe('createAccount', () => {
         const EMAIL = 'michael@las.com'
         it('should create account', () => {
@@ -48,8 +55,8 @@ describe('UserModule (e2e)', () => {
                     query: `
                         mutation {
                             createAccount(input: {
-                            email:"${EMAIL}",
-                            password:"12345",
+                            email:"${testUser.email}",
+                            password:"${testUser.password}",
                             role:Owner
                             }) {
                             ok
@@ -72,8 +79,8 @@ describe('UserModule (e2e)', () => {
                     query: `
                         mutation {
                             createAccount(input: {
-                            email:"${EMAIL}",
-                            password:"12345",
+                            email:"${testUser.email}",
+                            password:"${testUser.password}",
                             role:Owner
                             }) {
                             ok
@@ -90,8 +97,70 @@ describe('UserModule (e2e)', () => {
                 })
         })
     })
+
+    // 로그인 e2e 테스트
+    describe('login', () => {
+        it('should login with correct credentials', () => {
+            return request(app.getHttpServer())
+                .post(GRAPHQL_ENDPOINT)
+                .send({
+                    query: `
+                        mutation {
+                            login(input:{
+                            email:"${testUser.email}",
+                            password:"${testUser.password}",
+                            }) {
+                            ok
+                            error
+                            token
+                            }
+                        }
+                        `,
+                })
+                .expect(200)
+                .expect((res) => {
+                    const {
+                        body: {
+                            data: { login },
+                        },
+                    } = res
+                    expect(login.ok).toBe(true)
+                    expect(login.error).toBe(null)
+                    expect(login.token).toEqual(expect.any(String))
+                    jwtToken = login.token
+                })
+        })
+        it('should not be able to login with wrong credentials', () => {
+            return request(app.getHttpServer())
+                .post(GRAPHQL_ENDPOINT)
+                .send({
+                    query: `
+                        mutation {
+                            login(input:{
+                            email:"${testUser.email}",
+                            password:"xxx",
+                            }) {
+                            ok
+                            error
+                            token
+                            }
+                        }
+                        `,
+                })
+                .expect(200)
+                .expect((res) => {
+                    const {
+                        body: {
+                            data: { login },
+                        },
+                    } = res
+                    expect(login.ok).toBe(false)
+                    expect(login.error).toBe('Wrong password')
+                    expect(login.token).toBe(null)
+                })
+        })
+    })
     it.todo('userProfile')
-    it.todo('login')
     it.todo('me')
     it.todo('verifyEmail')
     it.todo('editProfile')
