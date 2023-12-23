64f1deac0d642c16c7cc8e9e1da165c7bac96478
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-12-24 00:00:01 +0900

    test: Me API E2E Test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -238,7 +238,55 @@ describe('UserModule (e2e)', () => {
                 })
         })
     })
-    it.todo('me')
+    // me는 로그인한 유저의 정보를 가져오는 쿼리이다.
+    describe('me', () => {
+        it('should find my profile', () => {
+            return request(app.getHttpServer())
+                .post(GRAPHQL_ENDPOINT)
+                .set('X-JWT', jwtToken)
+                .send({
+                    query: `
+                        {
+                            me {
+                            email
+                            }
+                        }
+                        `,
+                })
+                .expect(200)
+                .expect((res) => {
+                    const {
+                        body: {
+                            data: {
+                                me: { email },
+                            },
+                        },
+                    } = res
+                    expect(email).toBe(testUser.email)
+                })
+        })
+        it('should not allow logged out user', () => {
+            return request(app.getHttpServer())
+                .post(GRAPHQL_ENDPOINT)
+                .send({
+                    query: `
+                        {
+                        me {
+                            email
+                        }
+                        }
+                    `,
+                })
+                .expect(200)
+                .expect((res) => {
+                    const {
+                        body: { errors },
+                    } = res
+                    const [error] = errors
+                    expect(error.message).toBe('Forbidden resource')
+                })
+        })
+    })
     it.todo('verifyEmail')
     it.todo('editProfile')
 })
