9a81a70ce53c28bd00a1c36a96142ea8b1f5f2ee
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-01-08 23:01:57 +0900

    feat: EditRestaurant, @RelationId

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -2,7 +2,7 @@ import { Field, InputType, ObjectType } from '@nestjs/graphql'
 import { IsString, Length } from 'class-validator'
 import { CoreEntity } from 'src/common/entities/core.entity'
 import { User } from 'src/users/entities/user.entity'
-import { Column, Entity, ManyToOne } from 'typeorm'
+import { Column, Entity, ManyToOne, RelationId } from 'typeorm'
 import { Category } from './category.entity'
 
 @InputType('RestaurantInputType', { isAbstract: true })
@@ -32,4 +32,8 @@ export class Restaurant extends CoreEntity {
     @Field((type) => User)
     @ManyToOne((type) => User, (user) => user.restaurants, { onDelete: 'CASCADE' })
     owner: User
+
+    // RelationId 데코레이터는 DB에는 존재하지 않지만, Restaurant Entity에는 존재하는 ownerId를 가져올 수 있게 해준다.
+    @RelationId((restaurant: Restaurant) => restaurant.owner)
+    ownerId: number
 }
