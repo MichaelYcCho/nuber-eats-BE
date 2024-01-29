0bc07ecff3e799b41015405b238751db45289046
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-01-30 00:00:03 +0900

    feat: CreateOrder - Save Base Logic

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -27,9 +27,9 @@ export class Order extends CoreEntity {
     @ManyToOne((type) => User, (user) => user.rides, { onDelete: 'SET NULL', nullable: true })
     driver?: User
 
-    @Field((type) => Restaurant)
+    @Field((type) => Restaurant, { nullable: true })
     @ManyToOne((type) => Restaurant, (restaurant) => restaurant.orders, { onDelete: 'SET NULL', nullable: true })
-    restaurant: Restaurant
+    restaurant?: Restaurant
 
     @Field((type) => [OrderItem])
     @ManyToMany((type) => OrderItem)
@@ -41,7 +41,7 @@ export class Order extends CoreEntity {
     @IsNumber()
     total?: number
 
-    @Column({ type: 'enum', enum: OrderStatus })
+    @Column({ type: 'enum', enum: OrderStatus, default: OrderStatus.Pending })
     @Field((type) => OrderStatus)
     @IsEnum(OrderStatus)
     status: OrderStatus
